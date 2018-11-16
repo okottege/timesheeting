a0de8ad376e51fc99584255559877ce206de8d39
Oshan Kottege <oshan.kottege@gmail.com>
2018-11-16 21:42:28 +1300

    Initial commit.

---
+++ Timesheeting.Client.UI/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Blazor.Hosting;
+
+namespace Timesheeting.Client.UI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
+            BlazorWebAssemblyHost.CreateDefaultBuilder()
+                .UseBlazorStartup<Startup>();
+    }
+}

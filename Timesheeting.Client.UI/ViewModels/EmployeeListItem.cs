707220390fc154f769de602bf9a2f05d32dde81d
Oshan Kottege <oshan.kottege@gmail.com>
2018-11-29 21:41:54 +1300

    Started working on the employee list.

---
+++ Timesheeting.Client.UI/ViewModels/EmployeeListItem.cs
@@ -0,0 +1,9 @@
+﻿namespace Timesheeting.Client.UI.ViewModels
+{
+    public class EmployeeListItem
+    {
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
